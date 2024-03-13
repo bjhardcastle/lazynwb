cf87db7b8ddcd1ff227075ddba8e886f18eedf08
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-03-13 09:18:21 -0700

    Default to fsspec `anon=True` for testing

--- src/lazynwb/file_io.py
+++ src/lazynwb/file_io.py
@@ -5,10 +5,11 @@ import contextlib
 import h5py
 import npc_io
 import remfile
+import upath
 import zarr
 
 
-def open(path: npc_io.PathLike) -> h5py.File | zarr.Group:
+def open(path: npc_io.PathLike, anon: bool = True) -> h5py.File | zarr.Group:
     """
     Open a file that meets the NWB spec, minimizing the amount of data/metadata read.
 
@@ -20,8 +21,8 @@ def open(path: npc_io.PathLike) -> h5py.File | zarr.Group:
         >>> nwb = open('https://dandiarchive.s3.amazonaws.com/blobs/f78/fe2/f78fe2a6-3dc9-4c12-a288-fbf31ce6fc1c')
         >>> nwb = open('s3://codeocean-s3datasetsbucket-1u41qdg42ur9/39490bff-87c9-4ef2-b408-36334e748ac6/nwb/ecephys_620264_2022-08-02_15-39-59_experiment1_recording1.nwb')
     """
-    path = npc_io.from_pathlike(path)
-
+    path = upath.UPath(npc_io.from_pathlike(path), anon=anon)
+    
     # zarr ------------------------------------------------------------- #
     ## there's no file-name convention for what is a zarr file, so we have to try opening it and see if it works
     ## - zarr.open() is fast regardless of size
