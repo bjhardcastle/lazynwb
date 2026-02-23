d6ca9ec035712c4f4e2bb4dd64ae64d4b3c4bb64
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-02-23 13:27:29 -0800

    Fix internal path function location

--- src/lazynwb/base.py
+++ src/lazynwb/base.py
@@ -288,7 +288,7 @@ class LazyNWB:
         return {
             **self._to_dict(),
             **self.subject._to_dict(),
-            "paths": list(lazynwb.utils._traverse_internal_paths(self._file).keys()),
+            "paths": list(lazynwb.file_io.get_internal_paths(self._file._path).keys()),
         }
 
 
